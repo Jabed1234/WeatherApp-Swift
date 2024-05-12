b721e59124337f64e5204155c9c4b2138294dfb1
Jabed1234 <jmiah5@fordham.edu>
2024-05-12 00:18:13 -0400

    Initial Commit

---
+++ project2/project2App.swift
@@ -0,0 +1,17 @@
+//
+//  project2App.swift
+//  project2
+//
+//  Created by Jabed Miah on 5/12/24.
+//
+
+import SwiftUI
+
+@main
+struct project2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
